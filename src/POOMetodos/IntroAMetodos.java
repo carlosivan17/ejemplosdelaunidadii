5947342a92fab101808624c40ebfe31f56212289
CarlosDelgado <carlosdelgado1711@gmail.com>
2022-11-11 01:27:04 -0600

    se subio parte de la unidadii

---
+++ src/POOMetodos/IntroAMetodos.java
@@ -0,0 +1,13 @@
+package POOMetodos;
+
+public class IntroAMetodos {
+	public static void trabajar() {
+		System.out.println("Hola a introduccion a los metodos!");
+	}
+
+	public static void main(String[] args) {
+		trabajar();
+
+	}
+
+}
